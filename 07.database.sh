e1d6fc77dcc93d8a574db08f05d3569cb07d458f
manoj <manojmandy35@gmail.com>
2026-07-20 08:22:16 +0530

    07.database.sh

--- 07.database.sh
+++ 07.database.sh
@@ -3,8 +3,8 @@
 NUMBER1=100
 NUMBER2=200
 
-TIMESTAMP=$(DATE)
-echo "script executed at = $TIMESTAMP
+TIMESTAMP=$(date)
+echo "script executed at = $TIMESTAMP"
 
 SUM=$(($NUMBER1+$NUMBER2))
-echo "sum of $NUMBER1 & $NUMBER2 is = $SUM
+echo "sum of $NUMBER1 & $NUMBER2 is = $SUM"
