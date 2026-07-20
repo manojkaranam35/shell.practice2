2ce7674bbabb63b37ca893cf825312e251fe018e
manoj <manojmandy35@gmail.com>
2026-07-20 23:24:19 +0530

    08-arrays.sh

--- 08-arrays.sh
+++ 08-arrays.sh
@@ -6,7 +6,9 @@ echo "first friend= ${friends[0]}"
 
 echo "two friends= ${friends[@]:1:2}"
 
-echo "clg frnds=${friends[@]:2:3:4}"
+echo "clg frnds= ${friends[@]:2:3}"
+
+echo "home frnd= ${friends[0]} ${friends[2]} ${friends[4]}
 
 
 
