81a3ebb496e6d9d8d70fc78971c2fa900d9a4066
manoj <manojmandy35@gmail.com>
2026-07-21 23:02:18 +0530

    09-special-vars.sh

---
+++ 09-special-vars.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "All varaibles passed to the script: $@"
+echo "Number of varaibles: $#"
+
+echo "Script name: $0"
+echo "Current Directory: $PWD"
+echo "User running this script: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of the script: $$"
