7a0150ac85d21a5028679ca8172cc36fa68649a1
manoj <manojmandy35@gmail.com>
2026-07-23 22:59:51 +0530

    13-colors.sh

--- 13-colors.sh
+++ 13-colors.sh
@@ -1,56 +1,51 @@
 #!/bin/bash
 
-USERID=$(id -u)
+userid=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-if [ $USERID -ne 0 ]
+if [ $userid -ne 0 ]
 then
-    echo -e "$R ERROR:: Please run this script with root access $N"
-    exit 1 #give other than 0 upto 127
-else
-    echo "You are running with root access"
-fi
+  echo "show $R error...run the script with root access $N"
+  exit 1
+  else "you are running the script with root $Y access $N"
+  fi
 
-# validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo -e "Installing $2 is ... $G SUCCESS $N"
-    else
-        echo -e "Installing $2 is ... $R FAILURE $N"
-        exit 1
-    fi
+if [ $1 -eq 0 ]
+then
+  echo "installing $2 is $G success $N"
+else 
+  echo "installing $2 is $R failure $N"
+  fi
 }
 
 dnf list installed mysql
 if [ $? -ne 0 ]
 then
-    echo "MySQL is not installed... going to install it"
-    dnf install mysql -y
-    VALIDATE $? "MySQL"
+  echo "mysql $R not installed $N..$G going to install $N"
+  dnf install mysql
+  VALIDATE $? "mysql"
 else
-    echo -e "Nothing to do MySQL... $Y already installed $N"
+  echo "mysql already installed..$Y nothing to do $N"
 fi
-
-dnf list installed python3
-if [ $? -ne 0 ]
-then
-    echo "python3 is not installed... going to install it"
-    dnf install python3 -y
-    VALIDATE $? "python3"
-else
-    echo -e "Nothing to do python... $Y already installed $N"
-fi
-
 dnf list installed nginx
 if [ $? -ne 0 ]
 then
-    echo "nginx is not installed... going to install it"
-    dnf install nginx -y
-    VALIDATE $? "nginx"
+  echo "nginx $R not installed $N..$G going to install $N"
+  dnf install nginx
+  VALIDATE $? "nginx"
 else
-    echo -e "Nothing to do nginx... $Y already installed $N"
+  echo "nginx already installed..$Y nothing to do $N"
+fi
+dnf list installed httpd
+if [ $? -ne 0 ]
+then
+  echo "httpd $R not installed $N..$G going to install $N"
+  dnf install httpd
+  VALIDATE $? "httpd"
+else
+  echo "httpd already installed..$Y nothing to do $N"
 fi
