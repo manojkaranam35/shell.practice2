84cd0290e896334d186999c5971faffb18a01807
manoj <manojmandy35@gmail.com>
2026-07-23 23:13:11 +0530

    13-colors.sh

--- 13-colors.sh
+++ 13-colors.sh
@@ -8,44 +8,45 @@ N="\e[0m"
 
 if [ $userid -ne 0 ]
 then
-  echo "show $R error...run the script with root access $N"
+  echo -e "show $R error...run the script with root access $N"
   exit 1
-  else "you are running the script with root $Y access $N"
+  else
+  echo -e "you are running the script with root $Y access $N"
   fi
 
 VALIDATE(){
 if [ $1 -eq 0 ]
 then
-  echo "installing $2 is $G success $N"
+  echo -e "installing $2 is $G success $N"
 else 
-  echo "installing $2 is $R failure $N"
+  echo -e "installing $2 is $R failure $N"
   fi
 }
 
 dnf list installed mysql
 if [ $? -ne 0 ]
 then
-  echo "mysql $R not installed $N..$G going to install $N"
+  echo -e "mysql $R not installed $N..$G going to install $N"
   dnf install mysql
   VALIDATE $? "mysql"
 else
-  echo "mysql already installed..$Y nothing to do $N"
+  echo -e "mysql already installed..$Y nothing to do $N"
 fi
 dnf list installed nginx
 if [ $? -ne 0 ]
 then
-  echo "nginx $R not installed $N..$G going to install $N"
+  echo -e "nginx $R not installed $N..$G going to install $N"
   dnf install nginx
   VALIDATE $? "nginx"
 else
-  echo "nginx already installed..$Y nothing to do $N"
+  echo -e "nginx already installed..$Y nothing to do $N"
 fi
 dnf list installed httpd
 if [ $? -ne 0 ]
 then
-  echo "httpd $R not installed $N..$G going to install $N"
+  echo -e "httpd $R not installed $N..$G going to install $N"
   dnf install httpd
   VALIDATE $? "httpd"
 else
-  echo "httpd already installed..$Y nothing to do $N"
+  echo -e "httpd already installed..$Y nothing to do $N"
 fi
