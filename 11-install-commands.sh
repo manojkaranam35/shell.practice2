e8b402f7e9a5b15fbc5d51f7acf77430eb1dd6fc
manoj <manojmandy35@gmail.com>
2026-07-23 10:35:17 +0530

    11-install-commands.sh

--- 11-install-commands.sh
+++ 11-install-commands.sh
@@ -8,3 +8,18 @@ echo "error: Please run this script with root access"
 else
 echo " you are running script with root access"
 fi
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+   echo "mysql not installed..going to install"
+dnf install mysql
+if [ $? -eq 0 ]
+then
+      echo "installing mysql is....success"
+  else
+     echo "installing mysql is....failure"
+     exit1
+else
+    echo "mysql already installed"
+fi
