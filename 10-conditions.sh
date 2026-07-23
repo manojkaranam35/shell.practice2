89a6f7210e76bb3ba14db692cd6a2403c06c7737
manoj <manojmandy35@gmail.com>
2026-07-23 10:13:31 +0530

    10-conditions.sh

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -4,7 +4,7 @@ NUMBER=$1
 
 if [ $NUMBER -gt 15 ]
 then
-  echo "the NUMBER $NUMBER is not greater then 15"
-else
   echo "the NUMBER $NUMBER is greater then 15"
+else
+  echo "the NUMBER $NUMBER is not greater then 15"
 fi
