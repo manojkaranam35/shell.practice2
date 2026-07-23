6f6f0ecc3ef2363098c9c8a2888348c5ac7b4c27
manoj <manojmandy35@gmail.com>
2026-07-23 11:02:26 +0530

    11-install-commands.sh

--- 12.functions.sh
+++ 12.functions.sh
@@ -10,31 +10,31 @@ else
 fi
 
 VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo "Installing $2 is ... SUCCESS"
-    else
-        echo "Installing $2 is ... FAILURE"
-        exit 1
-    fi
+if [ $1 -eq 0 ]
+then
+   echo "installing $2 is..success"
+else
+   echo "installing $2 is...failure"
+ exit 1
+ fi
 }
 
 dnf list installed mysql
 if [ $? -ne 0 ]
 then
-    echo "MySQL is not installed... going to install it"
-    dnf install mysql -y
-    VALIDATE $? "MySQL"
+   echo "mysql not installed...going to istall"
+   dnf install mysql -y
+   VALIDATE $? "mysql"
 else
-    echo "MySQL is already installed...Nothing to do"
+   echo "mysql already installed..nothing to do"
 fi
 
-dnf list installed python3
+dnf list installed pytho3
 if [ $? -ne 0 ]
 then
-    echo "python3 is not installed... going to install it"
-    dnf install python3 -y
-    VALIDATE $? "python3"
+   echo "pytho3 not installed...going to istall"
+   dnf install pytho3 -y
+   VALIDATE $? "pytho3"
 else
-    echo "python3 is already installed...Nothing to do"
+   echo "pytho3 already installed..nothing to do"
 fi
