0294294d0073bbd580fb9913a2f1938494165d18
manoj <manojmandy35@gmail.com>
2026-07-23 10:50:59 +0530

    11-install-commands.sh

---
+++ 12.functions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+userid=(id -u)
+
+if [ $userid -ne 0]
+then
+    echo "error:run the script with root user"
+else
+    echo "you are running with root user"
+fi
+
+VALIDATE(){
+if [ $1 -eq 0 ]
+then
+   echo "installing $1 is..success"
+else
+   echo "installing $1 is...failure"
+ exit1
+ fi
+}
+
+dnf lint installed mysql
+if [ $? -ne 0 ]
+then
+   echo "mysql not installed...going to istall"
+   dnf install mysql -y
+   VALIDAT $? "mysql"
+else
+   echo "mysql already installed..nothing to do"
+fi
